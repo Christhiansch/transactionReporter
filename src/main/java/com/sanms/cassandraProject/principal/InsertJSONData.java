791a99bd024953a43f5a1a39b218cc92a58c681a
christhian.sandoval.contractor <christhian.sandoval.contractor@bbva.com>
2022-04-05 23:50:20 -0500

    Feat: Test files for executions

---
+++ src/main/java/com/sanms/cassandraProject/principal/InsertJSONData.java
@@ -0,0 +1,86 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.sanms.cassandraProject.principal;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.lang.reflect.Type;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+import com.google.gson.stream.JsonReader;
+import com.sanms.cassandraProject.model.ProcessingCode;
+
+/**
+ *
+ * @author sanms
+ */
+public class InsertJSONData
+{
+  
+	private static final Type PROCESSCODE_TYPE = new TypeToken<List<ProcessingCode>>() {}.getType();
+	
+  public static void main(String[] args)
+  {
+    
+	/**
+	 * Gson Test
+	 */
+    Gson gsontest = new Gson();
+    
+    ProcessingCode process1 = new ProcessingCode();
+    ProcessingCode process2 = new ProcessingCode();
+    
+    process1.setCod_operacion("011000");
+    process1.setDesc_operacion("RETIRO ATM");
+    process1.setTipo_operacion("RETIRO");
+    
+    process2.setCod_operacion("301000");
+    process2.setDesc_operacion("CONSULTA DE SALDO DEB");
+    process2.setTipo_operacion("CONSULTA DE SALDO");
+    
+    List<ProcessingCode> lstProcessCode = new ArrayList<>();
+    lstProcessCode.add(process1);
+    lstProcessCode.add(process2);
+    
+    
+    String json = gsontest.toJson(lstProcessCode);
+    
+    //Object to JSON
+    //System.out.print(json);
+    
+    /**
+     * Gson Reader
+     */
+    
+    try {
+    	    	
+    	Gson gsonreader = new Gson();
+    	Path path = Paths.get("C:\\csdevelopment\\cassandraProject\\codigo_operacion.json");
+    	JsonReader jsonreader = new JsonReader(new FileReader(path.toFile()));
+    	List<ProcessingCode> lstProcess = gsonreader.fromJson(jsonreader, PROCESSCODE_TYPE);
+    	
+    	for(ProcessingCode process: lstProcess) {
+    		System.out.println(process);
+    	}    	
+    	
+    	
+	} catch (FileNotFoundException e) {
+		// TODO Auto-generated catch block
+		e.printStackTrace();
+	}    
+    
+    
+  }
+  
+}
---
+++ src/main/java/com/sanms/cassandraProject/principal/Principal.java
@@ -0,0 +1,68 @@
+package com.sanms.cassandraProject.principal;
+
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.lang.reflect.Type;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
+
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+import com.google.gson.stream.JsonReader;
+import com.sanms.cassandraProject.CassandraConnector;
+import com.sanms.cassandraProject.model.ProcessingCode;
+import com.sanms.cassandraProject.util.ColumnCoin;
+import com.sanms.cassandraProject.util.ColumnProcessingCode;
+import com.sanms.cassandraProject.util.ColumnTrnMovement;
+import com.sanms.cassandraProject.util.Table;
+
+public class Principal {
+	
+	public static final String KEYSPACENAME = "TRANSACTION_REPORTER";
+	private static final Type PROCESSCODE_TYPE = new TypeToken<List<ProcessingCode>>() {}.getType();
+	
+	public static void main(String[] args) {
+		
+		CassandraConnector client = new CassandraConnector();
+		client.connect("127.0.0.1", 9042);
+		
+		client.createKeySpace(KEYSPACENAME, "SimpleStrategy", 1);
+		
+		//CREATE TABLE COIN
+		client.createTable(KEYSPACENAME, Table.COIN, ColumnCoin.getQueryColumnsCoin());
+		System.out.println("CREATED TABLE: " + Table.COIN);
+		
+		//CREATE TABLE PROCESSINGCODE
+		client.createTable(KEYSPACENAME, Table.PROCESSING_CODE, ColumnProcessingCode.getQueryColumnsProcessingCode());
+		System.out.println("CREATED TABLE: " + Table.PROCESSING_CODE);
+		
+		//CREATE TABLE TRANSACTION_MOVEMENT
+		client.createTable(KEYSPACENAME, Table.TRN_MOVEMNT, ColumnTrnMovement.getQueryColumnTrnMovemnt());
+		System.out.println("CREATED TABLE: " + Table.TRN_MOVEMNT);
+		
+		//INSERT DATA PROCESSINGCODE
+		
+		Gson gsonreader = new Gson();
+    	Path path = Paths.get("C:\\csdevelopment\\cassandraProject\\codigo_operacion.json");
+    	JsonReader jsonreader;
+    	
+		try {
+			
+			jsonreader = new JsonReader(new FileReader(path.toFile()));
+			List<ProcessingCode> lstProcess = gsonreader.fromJson(jsonreader, PROCESSCODE_TYPE);
+			client.insertDataProcessingCode(KEYSPACENAME, lstProcess, Table.PROCESSING_CODE);
+			
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+    	
+    	
+		//INSERT DATA TRANSACTION MOVEMENT
+		
+		
+		client.close();
+	}
+	
+}
